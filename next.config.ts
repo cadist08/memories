14a575369af14f66108fdcf821ae7102c472a958
cadist08 <zaenalf3314@gmail.com>
2026-02-17 22:30:52 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Wajib untuk GitHub Pages agar menghasilkan folder 'out'
-  
-  // Ganti '/love-story' dengan '/' + nama repository kamu di GitHub
-  // Contoh: Jika repo-mu namanya 'kado-nia', tulis '/kado-nia'
-  basePath: '/memories', 
-  
-  images: {
-    unoptimized: true, // Karena GitHub Pages tidak mendukung fitur optimasi gambar otomatis Next.js
-  },
+  /* config options here */
 };
 
 export default nextConfig;
